234794ebf147bc2b68b9026be9e7a83a22eb0b87
Osvaldo Valentin <ovalentindev@gmail.com>
2025-09-14 22:03:29 -0600

    feat(config): add title template to app head in nuxt config
    Add title template to display the page title with "- Aeto team sandbox" suffix for better branding and identification

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  app: {
+    head: {
+      titleTemplate: '%s - Aeto team sandbox',
+    },
+  },
+
   modules: [
     '@nuxt/eslint',
     '@nuxt/image',
